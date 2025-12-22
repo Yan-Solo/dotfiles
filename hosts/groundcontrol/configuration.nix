235dbdad576fa8e88533d586698d44deaf11f20c
jan <jan.peters1995@gmail.com>
2025-12-22 15:16:23 +0100

    groundcontrol

--- hosts/groundcontrol/configuration.nix
+++ hosts/groundcontrol/configuration.nix
@@ -2,12 +2,19 @@
 
 {
   config = {
+    system.stateVersion = "25.11";
+
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
+    boot.plymouth.enable = true;
 
-    system.stateVersion = "25.11";
+    systemSettings = {
+      users = [ "jan" ];
+      adminUsers = [ "jan" ];
+      powerprofiles = false;
+    };
 
     home-manager.users.jan = {
       home.stateVersion = "25.11";
